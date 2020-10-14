676520b99bec961bf7adb6445e49cadd0bfcacda
missou <695383649@qq.com>
2020-10-14 21:46:43 +0800

    优化org-mode图片显示

--- lisp/orgmode.el
+++ lisp/orgmode.el
@@ -1,5 +1,6 @@
 ;; org-mode配置
 (setq org-bullets-bullet-list '("☰" "☷" "✿" "☭"))
 (add-hook 'org-mode-hook 'org-bullets-mode)
+(setq org-image-actual-width (/ (display-pixel-width) 3))
 
 (provide 'orgmode)
