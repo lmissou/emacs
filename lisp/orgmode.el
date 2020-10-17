25b092ce9119060cf75b31f834897380faff37ef
missou <695383649@qq.com>
2020-10-17 22:05:40 +0800

    org-mode添加babel配置

--- lisp/orgmode.el
+++ lisp/orgmode.el
@@ -2,5 +2,14 @@
 (setq org-bullets-bullet-list '("☰" "☷" "✿" "☭"))
 (add-hook 'org-mode-hook 'org-bullets-mode)
 (setq org-image-actual-width (/ (display-pixel-width) 3))
+;; org-babel配置
+(org-babel-do-load-languages
+      'org-babel-load-languages
+      '((emacs-lisp . t)
+	(shell . t)
+        (python . t)
+        (js . t)
+	(latex . t)
+        ))
 
 (provide 'orgmode)
