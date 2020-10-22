5d6ea6a040a108f397ca04097c73be6f67273d09
missou <695383649@qq.com>
2020-10-22 23:41:06 +0800

    中英文等宽设置

--- lisp/init-funcs.el
+++ lisp/init-funcs.el
@@ -1,21 +1,16 @@
 (require 'cl)
-;; ;; 判断包是否已经安装
-;; (defun my/packages-installed-p ()
-;;   (loop for pkg in my/packages
-;; 	when (not (package-installed-p pkg)) do (return nil)
-;; 	finally (return t)))
-;; ;; 自动安装未安装的包
-;; (defun my/packages-init ()
-;;   (interactive)
-;;   (unless (my/packages-installed-p)
-;;     (message "%s" "Refreshing package database...")
-;;     (package-refresh-contents)
-;;     (dolist (pkg my/packages)
-;;       (when (not (package-installed-p pkg))
-;; 	(package-install pkg)))))
+
 ;; 开关内置终端
 (defun my/multi-term-dedicated-toggle-select () (interactive) (progn (multi-term-dedicated-toggle)
-  (when (multi-term-dedicated-exist-p)
-  (multi-term-dedicated-select))))
+								     (when (multi-term-dedicated-exist-p)
+								       (multi-term-dedicated-select))))
+;; 中文与外文字体设置
+(defun my/set-font (english chinese english-size chinese-size)
+  "分别设置英文和中文字体达到中英文等宽"
+  (set-face-attribute 'default nil :font
+                      (format   "%s:pixelsize=%d"  english english-size))
+  (dolist (charset '(kana han symbol cjk-misc bopomofo))
+    (set-fontset-font (frame-parameter nil 'font) charset
+                      (font-spec :family chinese :size chinese-size))))
 
 (provide 'init-funcs)
