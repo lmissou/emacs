b7b23875069bdafb12b74ace68410455bfc4068e
missou <695383649@qq.com>
2020-10-25 22:03:16 +0800

    优化配置；增加有道词典翻译插件

---
+++ lisp/init-tool.el
@@ -0,0 +1,53 @@
+;; 常用工具
+
+;; 全局开启undo-tree优化重做撤销
+(use-package undo-tree
+  :config
+  (global-undo-tree-mode t))
+
+;; 删除选中的内容
+(use-package hungry-delete
+  :config
+  (global-hungry-delete-mode t))
+
+;; 窗口跳转
+(use-package ace-window
+  :config
+  (setq aw-keys '(?a ?s ?d ?f ?g ?h ?j ?k ?l))
+  :bind (("M-o" . ace-window)))
+
+;; 快速选择
+(use-package expand-region
+  :bind (("C-=" . er/expand-region)))
+
+;; 多光标
+(use-package multiple-cursors
+  :bind (("C-S-<mouse-1>" . mc/add-cursor-on-click)
+	 ("C-M-p" . mc/mmlte--up)
+	 ("C-M-n" . mc/mmlte--down)))
+
+;; 快速跳转
+(use-package avy)
+
+;; treemacs文件目录树
+(use-package treemacs
+  :bind (:map treemacs-mode-map
+	      ("j" . treemacs-next-line)
+	      ("k" . treemacs-previous-line))
+  :config
+  (treemacs-resize-icons 14)
+  ;; treemacs的all-the-icons主题
+  (use-package treemacs-all-the-icons
+    :config
+    (require 'treemacs-all-the-icons)
+    (treemacs-load-theme "all-the-icons"))
+  (use-package treemacs-magit)
+  (use-package treemacs-projectile))
+
+;; 有道词典
+(use-package youdao-dictionary
+  :bind ("C-c y" . youdao-dictionary-search-at-point)
+  :config
+  (setq url-automatic-caching t))
+
+(provide 'init-tool)
